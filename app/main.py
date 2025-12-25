a48baa484bc461098c190ca0b8faf02786572833
Kor9th <kanemane17@proton.me>
2025-12-25 11:03:46 -0500

    Fix CORS handling and backend config

--- app/main.py
+++ app/main.py
@@ -18,12 +18,11 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origin_regex=r"^http://(localhost|127\.0\.0\.1)(:\d+)?$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 models.Base.metadata.create_all(bind=engine)
 
